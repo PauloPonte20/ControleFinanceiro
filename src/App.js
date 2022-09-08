2ef4c98c3aad36d48e0c175dcac97786ff0dbcdc
Paulo Ponte <gabeponte20@gmail.com>
2022-09-08 19:13:31 -0300

    createGlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import GlobalStyle from "./Styles/globals";
 
 const App = () => {
-    return <div>App</div>;
+  return (
+    <>
+      <div>App</div>
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
